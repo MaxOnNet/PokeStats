677f0c72e01867d6c5c030c52618482cb3802de0
Vitkor Tatarnikov <viktor@tatarnikov.org>
2016-07-22 15:59:08 +0600

    Test Flask

--- runserver.py
+++ runserver.py
@@ -4,6 +4,7 @@
 import os
 import logging
 
+from Interfaces.Config import Config
 from threading import Thread
 
 from pogom import config
@@ -13,54 +14,48 @@ from pogom.search import search_loop
 from pogom.models import create_tables, Pokemon
 from pogom.pgoapi.utilities import get_pos_by_name
 
+config_xml = Config()
 log = logging.getLogger(__name__)
+debug=False
 
-
-def start_locator_thread(args):
-    search_thread = Thread(target=search_loop, args=(args,))
+def start_locator_thread(config_xml):
+    search_thread = Thread(target=search_loop, args=(config_xml,))
     search_thread.daemon = True
     search_thread.name = 'search_thread'
     search_thread.start()
 
+create_tables()
+position = get_pos_by_name(config_xml.get("map", "", "location", "Омск, Менделеева, 21"))
+config['ORIGINAL_LATITUDE'] = position[0]
+config['ORIGINAL_LONGITUDE'] = position[1]
+config['GMAPS_KEY'] = config_xml.get("map", "google", "key", "")
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(module)11s] [%(levelname)7s] %(message)s')
 
-    logging.getLogger("peewee").setLevel(logging.INFO)
-    logging.getLogger("requests").setLevel(logging.WARNING)
-    logging.getLogger("pogom.pgoapi.pgoapi").setLevel(logging.WARNING)
-    logging.getLogger("pogom.pgoapi.rpc_api").setLevel(logging.INFO)
+logging.getLogger("peewee").setLevel(logging.INFO)
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("pogom.pgoapi.pgoapi").setLevel(logging.WARNING)
+logging.getLogger("pogom.pgoapi.rpc_api").setLevel(logging.INFO)
 
-    args = get_args()
 
-    if args.debug:
-        logging.getLogger("requests").setLevel(logging.DEBUG)
-        logging.getLogger("pgoapi").setLevel(logging.DEBUG)
-        logging.getLogger("rpc_api").setLevel(logging.DEBUG)
+if debug:
+    logging.getLogger("requests").setLevel(logging.DEBUG)
+    logging.getLogger("pgoapi").setLevel(logging.DEBUG)
+    logging.getLogger("rpc_api").setLevel(logging.DEBUG)
 
-    create_tables()
 
-    position = get_pos_by_name(args.location)
-    log.info('Parsed location is: {:.4f}/{:.4f}/{:.4f} (lat/lng/alt)'.
+
+
+log.info('Parsed location is: {:.4f}/{:.4f}/{:.4f} (lat/lng/alt)'.
              format(*position))
 
-    config['ORIGINAL_LATITUDE'] = position[0]
-    config['ORIGINAL_LONGITUDE'] = position[1]
 
-    if args.ignore:
-        Pokemon.IGNORE = [i.lower().strip() for i in args.ignore.split(',')]
-    elif args.only:
-        Pokemon.ONLY = [i.lower().strip() for i in args.only.split(',')]
 
-    if not args.mock:
-        start_locator_thread(args)
-    else:
-        insert_mock_data(args.location, 6)
+#    if args.ignore:
+#        Pokemon.IGNORE = [i.lower().strip() for i in args.ignore.split(',')]
+#    elif args.only:
+#        Pokemon.ONLY = [i.lower().strip() for i in args.only.split(',')]
 
-    app = Pogom(__name__)
-    config['ROOT_PATH'] = app.root_path
-    if args.gmaps_key is not None:
-        config['GMAPS_KEY'] = args.gmaps_key
-    else:
-        config['GMAPS_KEY'] = load_credentials(os.path.dirname(os.path.realpath(__file__)))['gmaps_key']
-    app.run(threaded=True, host="127.0.0.1", port="5000")
+#    if not args.mock:
+start_locator_thread(config_xml)
