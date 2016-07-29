48ffb48bddf575aa7ebcae31aeb331e7761d16c9
Vitkor Tatarnikov <viktor@tatarnikov.org>
2016-07-30 00:54:54 +0600

    Add Pokedex and Rarity

--- Interfaces/AI/Worker/__init__.py
+++ Interfaces/AI/Worker/__init__.py
@@ -3,5 +3,6 @@
 #from EvolveAll import EvolveAll
 from SeenPokestop import SeenPokestop
 from MoveToPokestop import MoveToPokestop
+from MoveToGym import MoveToGym
 #from InitialTransfer import InitialTransfer
 #from PokemonCatch import PokemonCatch
