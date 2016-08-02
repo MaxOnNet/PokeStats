ba885a17d4b4b1dea0aa5da947060d7fadc67b3b
Vitkor Tatarnikov <viktor@tatarnikov.org>
2016-08-02 15:08:52 +0600

    Rebuild reports templates

--- Reports/Gyms/Team.py
+++ Reports/Gyms/Team.py
@@ -12,16 +12,39 @@ class Team(Report, View):
 
     sql_report = """
         SELECT
-            t.`id` as "team_id",
-            t.`name` as "team_name",
-            count(g.id) as "gym_count"
+            tm.`id`         as "team_id",
+            tm.`name`       as "team_name",
+            g.count     as "gym_count",
+            t.count    as "trainer_count"
         FROM
-            db_pokestats.gym as g,
-            db_pokestats.team as t
-        where
-            g.cd_team = t.id
-        GROUP BY t.id
-        ORDER BY count(g.id) DESC;
+            db_pokestats.team      as tm
+            LEFT JOIN
+            (
+				SELECT
+					tm.`id`         as "team_id",
+                    count(g.id)     as "count"
+				FROM
+					db_pokestats.team      as tm,
+                    db_pokestats.gym		as g
+				WHERE
+					g.cd_team = tm.id
+				GROUP BY tm.`id` ) g on (g.team_id = tm.id)
+            LEFT JOIN
+            (
+				SELECT
+					tm.`id`         as "team_id",
+                    count(t.id)     as "count"
+				FROM
+					db_pokestats.team      as tm,
+                    db_pokestats.trainer		as t
+				WHERE
+					t.cd_team = tm.id
+				GROUP BY tm.`id` ) t on (t.team_id = tm.id)
+        WHERE
+                tm.id = g.team_id
+            and tm.id = t.team_id
+        GROUP BY tm.id
+        ORDER BY g.count DESC;
     """
 
     def __init__(self, config):
@@ -38,7 +61,8 @@ class Team(Report, View):
             row_dict = {
                 "team_id": row[0],
                 "team_name": row[1],
-                "gym_count": row[2]
+                "gym_count": row[2],
+                "trainer_count": row[3]
             }
             self.data.append(row_dict)
 
