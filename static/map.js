774a26516a2238935df5871819a7be88f7184c47
Vitkor Tatarnikov <viktor@tatarnikov.org>
2016-08-05 12:13:38 +0600

    Add HTML5 Geopositions

--- static/map.js
+++ static/map.js
@@ -71,6 +71,7 @@ $selectNotify.on("change", function (e) {
 });
 
 var map;
+var marker;
 
 var light2Style=[{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}];
 var darkStyle=[{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#b39964"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#181818"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"lightness":17},{"color":"#525252"}]}];
@@ -110,6 +111,15 @@ function initMap() {
         },
     });
 
+    marker = new google.maps.Marker({
+        position: {
+            lat: center_lat,
+            lng: center_lng
+        },
+        map: map,
+        animation: google.maps.Animation.DROP
+    });
+
 	var style_dark = new google.maps.StyledMapType(darkStyle, {name: "Dark"});
 	map.mapTypes.set('dark_style', style_dark);
 
@@ -131,35 +141,27 @@ function initMap() {
 
     var infoWindow = new google.maps.InfoWindow({map: map});
 
-      // Try HTML5 geolocation.
-      if (navigator.geolocation) {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('Location found.');
-          map.setCenter(pos);
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+            marker.setPosition(pos);
         }, function() {
-          handleLocationError(true, infoWindow, map.getCenter());
+            handleLocationError(true, infoWindow, map.getCenter());
         });
-      } else {
+    } else {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
-      }
     }
-/*
-    marker = new google.maps.Marker({
-        position: {
-            lat: center_lat,
-            lng: center_lng
-        },
-        map: map,
-        animation: google.maps.Animation.DROP
-    });
-*/
+
+
     initSidebar();
 };
 
@@ -431,6 +433,9 @@ function updateMap() {
     localStorage.showPokestops = localStorage.showPokestops || false;
     localStorage.showScanned = localStorage.showScanned || false;
 
+    localPosition = map.getCenter()
+    localBounds.getBounds()
+
     $.ajax({
         url: "raw_data",
         type: 'GET',
@@ -438,7 +443,13 @@ function updateMap() {
             'pokemon': localStorage.showPokemon,
             'pokestops': localStorage.showPokestops,
             'gyms': localStorage.showGyms,
-            'scanned': localStorage.showScanned
+            'scanned': localStorage.showScanned,
+            'latitude': localPosition.lat(),
+            'longtude': localPosition.lng(),
+            'ne_latitude': localBounds.getNorthEast().lat(),
+            'ne_longtude': localBounds.getNorthEast().lng(),
+            'sw_latitude': localBounds.getSouthWest().lat(),
+            'sw_longtude': localBounds.getSouthWest().lng(),
         },
         dataType: "json"
     }).done(function(result) {
